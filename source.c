483f8e98b2f3aa711505ebe28beeb2f68cca83b4
test <Fozan Shahid>
2021-01-06 18:14:53 +0500

    second test

--- source.c
+++ source.c
@@ -2,7 +2,7 @@
 int main() {
    // printf() displays the string inside quotation
 	printf("Hello, World!");
-	printf("Hello sahriq");
+	printf("Hello sahriq"); //change by shariq
 	printf("hello from Fozan"); //change by fozan
    return 0;
 }
